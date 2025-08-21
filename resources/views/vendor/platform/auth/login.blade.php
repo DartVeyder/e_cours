48d69817471e71eee69b3a7a0afc32ffe7ed5620
progr <medvsd.dmitro@gmail.com>
2025-08-21 13:48:21 +0300

    update

--- resources/views/vendor/platform/auth/login.blade.php
+++ resources/views/vendor/platform/auth/login.blade.php
@@ -4,19 +4,6 @@
 @section('content')
     <div class="d-flex flex-column align-items-center">
         <h1 class="h4 text-body-emphasis mb-4">{{__('Sign in to your account')}}</h1>
-        <form class="m-t-md"
-              role="form"
-              method="POST"
-              data-controller="form"
-              data-form-need-prevents-form-abandonment-value="false"
-              data-action="form#submit"
-              action="{{ route('platform.login.auth') }}">
-            @csrf
-
-            @includeWhen($isLockUser,'platform::auth.lockme')
-            @includeWhen(!$isLockUser,'platform::auth.signin')
-        </form>
-
         <a href="/auth/google/redirect"><img src=" {{asset('/images/login_google.svg')}}" alt=""></a>
         @foreach ($errors->all() as $error)
 
